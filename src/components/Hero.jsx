7c10bcad35c85600a0ced98757eb5ce6dd0034ae
awan-huzaifa <huzaifa.m.awan@gmail.com>
2023-06-26 23:22:01 +0500

    Update GitHubButton component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,8 @@ const Hero = () => {
   <div className='w-3/4 flex items-center justify-between mt-4'>
     
   <img src={logo} alt="logo" />
-    <button className="rounded-full bg-orange-800 px-8 py-4 text-slate-950 font-bold text-base tracking-wide">GitHub</button>
+  <a href='https://github.com/awan-huzaifa/Articel-Summarizer' target="_blank" rel="noreferrer">
+    <button className="rounded-full bg-orange-800 px-8 py-4 text-slate-950 font-bold text-base tracking-wide">GitHub</button></a>
   </div>
   {/* ............ */}
   <div className='w-3/4 text-center mt-12' >
@@ -15,11 +16,7 @@ const Hero = () => {
    <h1 className='text-orange-800 font-bold text-5xl pt-4'>OpenAI GPT-4</h1>
    <p className='text-slate-950 pt-4 text-xl'>Simplify your reading with Summize, an open-source article summarizer that transforms lengthy articles into clear and concise summaries.</p>
   </div>
-  {/* ........... */}
-  <div></div>
-  {/* .............. */}
-  <div></div>
-  {/* ................. */}
+  
     </header>
   )
 }
